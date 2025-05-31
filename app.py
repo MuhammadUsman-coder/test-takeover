6783d976190c7114042d4e52428a956040fdbca1
UsmanMansha <54319802+MuhammadUsman-coder@users.noreply.github.com>
2025-05-31 14:35:13 +0500

    Update app.py

--- app.py
+++ app.py
@@ -2,5 +2,17 @@ from flask import Flask
 app = Flask(__name__)
 
 @app.route("/poc")
-def hello():
+def poc():
     return "Takeover POC by SecurityReapers"
+
+@app.route("/")
+def hidden():
+    return """
+    <html>
+        <head><title>SecurityReapers</title></head>
+        <body>
+            <!-- Takeover POC by SecurityReapers -->
+            <p>Welcome</p>
+        </body>
+    </html>
+    """
